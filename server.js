257dfece3dba39b0eab6adf21e0f0e2e2794e84c
invitaendigitalweb-svg <invitaendigitalweb@gmail.com>
2026-08-24 17:10:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -90,7 +90,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
